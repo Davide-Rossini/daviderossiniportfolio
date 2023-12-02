729768a96aa34533d4704897b87f3ac5776307ae
DavozGamer <davo.z.gam3r@gmail.com>
2023-12-02 16:51:32 +0100

    minor changes

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -9,7 +9,9 @@ const Intro = () => {
             <div className="introContent">
                 <span className="hello">Hello,</span>
                 <span className="introText">I'm <span className="introName">Davide</span><br />Website Designer</span>
-                <p>I am a web developer and designer, proficient in creating visually appealing and functional websites with a strong foundation in coding and design principles.</p>
+                <p>I am a web developer and designer,</p>
+                <p>proficient in creating visually appealing and functional </p>
+                <p>websites with a strong foundation in coding and design principles.</p>
                 <Link><button className="btn">Contact me</button></Link>
             </div>
             <img src={bg} alt="Profile" className="bg"></img>
--- src/components/NavBar/navbar.js
+++ src/components/NavBar/navbar.js
@@ -7,14 +7,14 @@ import { Button, Link } from "react-scroll";
 export const Navbar = () => {
     return (
         <nav className="navbar">
-            <img src={logo} alt="Logo" className="logo" />
+            <img src={logo} alt="Logo" className="logo" draggable="false"/>
             <div className="desktopMenu">
                 <Link className="desktopMenuListItem">Home</Link>
                 <Link className="desktopMenuListItem">About</Link>
                 <Link className="desktopMenuListItem">Contact me</Link>
             </div>
             <button className="desktopMenuBtn">
-                <img src={github} alt="" className="desktopMenuImg"></img>
+                <img src={github} alt="" className="desktopMenuImg" draggable="false"></img>
             </button>
         </nav>
     );
