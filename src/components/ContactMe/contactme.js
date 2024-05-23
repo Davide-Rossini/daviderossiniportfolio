c0a79872f45c3f07f4072f467a171d4262e704d7
DavozGamer <davo.z.gam3r@gmail.com>
2024-05-23 17:51:02 +0200

    contact me icons added and open page

--- src/components/ContactMe/contactme.js
+++ src/components/ContactMe/contactme.js
@@ -1,6 +1,9 @@
 import React from "react";
 import "./contactme.css";
 import { Element } from "react-scroll";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLinkedin, faInstagram } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 function Contactme() {
     return (
@@ -8,37 +11,15 @@ function Contactme() {
             <Element name="contactSection">
                 <h1 className="title">Contact Me</h1>
                 <section className="contactme_sez">
-                    <hr />
-                    <form id="contacts">
-                        <label htmlFor="email">Email:</label>
-                        <br />
-                        <input type="email" id="email" name="email" required />
-                        <br />
-                        <label htmlFor="phone">Phone:</label>
-                        <br />
-                        <input
-                            type="tel"
-                            id="phone"
-                            name="phone"
-                            pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
-                            required
-                            maxLength="10"
-                            onKeyPress={(event) => {
-                                const charCode = event.which
-                                    ? event.which
-                                    : event.keyCode;
-                                if (
-                                    charCode > 31 &&
-                                    (charCode < 48 || charCode > 57)
-                                ) {
-                                    event.preventDefault();
-                                }
-                            }}
-                        />
-                        <small>Format: 123-456-7890</small>
-                        <br />
-                        <input className="submit-btn" type="submit" value="Submit" />
-                    </form>
+                    <a href="https://www.linkedin.com/in/davide-rossini-/" target="_blank" rel="noopener noreferrer" className="contact_icon" id="linkedin">
+                        <FontAwesomeIcon icon={faLinkedin} />
+                    </a>
+                    <a href="https://www.instagram.com/davide__rossini__/" target="_blank" rel="noopener noreferrer" className="contact_icon" id="instagram">
+                        <FontAwesomeIcon icon={faInstagram} />
+                    </a>
+                    <a href="mailto:rossini.davide709@gmail.com" className="contact_icon" id="email">
+                        <FontAwesomeIcon icon={faEnvelope} />
+                    </a>
                 </section>
             </Element>
         </div>
